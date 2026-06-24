248eba629c467663cdc8def4f5f1473d14ca4cfd
VipulPhatanagare <vipulphatangare3@gmail.com>
2026-06-24 16:58:04 +0530

    chage the ui

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://aichatbot.vipulphatangare.site/api";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://chatbotmanagement.vipulphatangare.cloud/api";
 
 export const apiClient = axios.create({
   baseURL: API_BASE_URL,
